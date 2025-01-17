4f7ccee596146acc6e03416e9aecc147dfda4753
lucianossouza <luciano.illustrador@hotmail.com>
2025-01-17 19:41:40 -0300

    feat: create StepPassword component

---
+++ src/components/StepPassword.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="input-container">
+    <label for="passwoard">Sua senha</label>
+    <input
+      id="password"
+      type="password"
+      v-model="data.password"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  data: {
+    password: String,
+  },
+});
+</script>
+
+<style>
+.input-container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+}
+label {
+  margin-bottom: 12px;
+  margin-right: 10px;
+}
+input,
+select {
+  padding: 5px 10px;
+  border-radius: 5px;
+  margin-right: 8px;
+}
+</style>
