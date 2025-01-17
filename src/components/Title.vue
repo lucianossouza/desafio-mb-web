afa4b6e1d09746f7b8caa6591111fe22990f8b07
lucianossouza <luciano.illustrador@hotmail.com>
2025-01-17 19:20:17 -0300

    feat: create Title component

---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<script setup>
+defineProps({
+  text: {
+      type: String,
+      required: true,
+    },
+});
+</script>
+
+<style scoped>
+h1 {
+  font-size: 24px;
+  color: #333;
+}
+</style>
