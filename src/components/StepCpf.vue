437b73e5d4dc9b508dd4c933f326024d5fabe4d7
lucianossouza <luciano.illustrador@hotmail.com>
2025-01-17 19:35:27 -0300

    feat: create StepCpf component

---
+++ src/components/StepCpf.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="input-container">
+    <label for="nome">Nome</label>
+    <input type="text" v-model="data.name" id="nome">
+  </div>
+  <div class="input-container">
+    <label for="cpf">CPF</label>
+    <input type="number" v-model="data.cpf" id="cpf">
+  </div>
+  <div class="input-container">
+    <label for="data">Data de nascimento</label>
+    <input type="data" v-model="data.birthdate" id="data">
+  </div>
+  <div class="input-container">
+    <label for="data">Telefone</label>
+    <input type="phone" v-model="data.phone" id="phone">
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  data: {
+    name: String,
+    cpf: String,
+    birthdate: String,
+    phone: String,
+  }
+});
+</script>
