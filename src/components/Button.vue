ef25c3f25a5ce8f802e922cbe6523501cd2bc354
lucianossouza <luciano.illustrador@hotmail.com>
2025-01-17 19:23:14 -0300

    feat: create Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button :class="['btn', customClass]" :type="type" @click="handleClick">
+    <slot />
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  handleClick: Function,
+  type: {
+    type: String,
+    default: "button",
+  },
+  customClass: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<style>
+.btn {
+  width: 160px;
+  background-color: orange;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
